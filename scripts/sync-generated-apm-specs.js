b5a70e0e003c3f4f65358a318cbe375c31c28359
Andrew Berestov <andrii.berestov@checkout.com>
2022-07-04 17:36:35 +0300

    [APM-4973] Add Giropay/EPS NAS Request/Response source

--- scripts/sync-generated-apm-specs.js
+++ scripts/sync-generated-apm-specs.js
@@ -4,3 +4,9 @@ var specs = require('./sync-specs');
 
 specs.syncPaymentRequest('Sofort', 'https://apm-sofort.cko-sbox.ckotech.co/relations/gw/pay');
 specs.syncPaymentResponse('Sofort', 'https://apm-sofort.cko-sbox.ckotech.co/relations/gw/payment');
+
+specs.syncPaymentRequest('Giropay', 'https://apm-giropay.cko-sbox.ckotech.co/giropay/relations/gw/pay');
+specs.syncPaymentResponse('Giropay', 'https://apm-giropay.cko-sbox.ckotech.co/giropay/relations/gw/payment');
+
+specs.syncPaymentRequest('EPS', 'https://apm-giropay.cko-sbox.ckotech.co/eps/relations/gw/pay');
+specs.syncPaymentResponse('EPS', 'https://apm-giropay.cko-sbox.ckotech.co/eps/relations/gw/payment');
