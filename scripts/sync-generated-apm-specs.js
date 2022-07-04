42d8d32912326d34b8e03ba0505eac5fcdbd5667
Andrew Berestov <andrii.berestov@checkout.com>
2022-07-04 16:22:07 +0300

    [APM-4974] Add PPro sources

--- scripts/sync-generated-apm-specs.js
+++ scripts/sync-generated-apm-specs.js
@@ -2,5 +2,20 @@
 
 var specs = require('./sync-specs');
 
-specs.syncPaymentRequest('Sofort', 'https://apm-sofort.cko-sbox.ckotech.co/relations/gw/pay');
-specs.syncPaymentResponse('Sofort', 'https://apm-sofort.cko-sbox.ckotech.co/relations/gw/payment');
+// specs.syncPaymentRequest('Sofort', 'https://apm-sofort.cko-sbox.ckotech.co/relations/gw/pay');
+// specs.syncPaymentResponse('Sofort', 'https://apm-sofort.cko-sbox.ckotech.co/relations/gw/payment');
+
+specs.syncPaymentRequest('Bancontact', 'https://apm-ppro.cko-sbox.ckotech.co/bancontact/relations/gw/pay');
+specs.syncPaymentResponse('Bancontact', 'https://apm-ppro.cko-sbox.ckotech.co/bancontact/relations/gw/payment');
+
+
+specs.syncPaymentRequest('P24', 'https://apm-ppro.cko-sbox.ckotech.co/p24/relations/gw/pay');
+specs.syncPaymentResponse('P24', 'https://apm-ppro.cko-sbox.ckotech.co/p24/relations/gw/payment');
+
+
+specs.syncPaymentRequest('Postfinance', 'https://apm-ppro.cko-sbox.ckotech.co/postfinance/relations/gw/pay');
+specs.syncPaymentResponse('Postfinance', 'https://apm-ppro.cko-sbox.ckotech.co/postfinance/relations/gw/payment');
+
+
+specs.syncPaymentRequest('Multibanco', 'https://apm-ppro.cko-sbox.ckotech.co/multibanco/relations/gw/pay');
+specs.syncPaymentResponse('Multibanco', 'https://apm-ppro.cko-sbox.ckotech.co/multibanco/relations/gw/payment');
